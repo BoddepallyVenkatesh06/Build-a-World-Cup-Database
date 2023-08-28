6d2614784dbc8725ff5c0295a6bfcbb04376ddbd
venkykumar0006 <65965126+BoddepallyVenkatesh06@users.noreply.github.com>
2023-08-29 01:39:43 +0530

    Create queries.sh

---
+++ queries.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Query to get all teams
+mysql -u your_username -p your_database_name -e "SELECT * FROM Teams;"
+
+# Query to get all matches
+mysql -u your_username -p your_database_name -e "SELECT * FROM Matches;"
+
+# Add more example queries
